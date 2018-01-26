bc0f301149ea159469f6a5b9aa42a5376b8fedcc
Gerardo Herrera <gherrer1@binghamton.edu>
2018-01-26 17:47:28 -0500

    excel2json done, heavily relied on another npm module but its fine, needed a good unit testing warmup

--- src/excelToJSON.js
+++ src/excelToJSON.js
@@ -1,5 +1,9 @@
-module.exports = function() {
-  return function() {
-    
+module.exports = function(engine) {
+  return function(filename) {
+    if(!filename.match(/\.xlsx$/))
+      throw new Error('InvalidFileTypeError')
+    const workbook = engine.readFile(filename);
+    const sheet = workbook.Sheets.sheet1;
+    return engine.utils.sheet_to_json(sheet);
   }
 }
--- test/excelToJSON.test.js
+++ test/excelToJSON.test.js
@@ -1,23 +1,39 @@
 const assert = require('chai').assert;
+const xlsx = require('xlsx');
 const excelToJSON = require('../src/excelToJSON');
 
 describe('excelToJSON', function() {
   describe('config', function() {
-    it('should return a function when passed in a csv-reader-engine (ex: excel module)', function() {
-      const excel = require('excel');
-      let excel2JSON = excelToJSON(excel);
+    it('should return a function when passed in a csv-reader-engine (ex: xlsx module)', function() {
+      let excel2JSON = excelToJSON(xlsx);
       assert.isFunction(excel2JSON, 'Expected excelToJSON to return a function');
     });
   });
   describe('use', function() {
-    it('should throw error if file passed in doesnt end with .xlsx', function() {
-      throw new Error('red-green refactor');
+    let excel2JSON;
+    beforeEach(function() {
+      excel2JSON = excelToJSON(xlsx);
     });
-    it('should not throw error if file passed in ends with .xlsx', function() {
-      throw new Error('red-green refactor');
+    it('should throw InvalidFileTypeError if file passed in doesnt end with .xlsx', function() {
+      const invalidFileName = 'haha'
+      assert.throws(excel2JSON.bind(null, invalidFileName), /InvalidFileTypeError/);
     });
-    it('should return an object(?)', function() {
-      throw new Error('red-green refactor');
+    it('should not throw InvalidFileError if file passed in ends with .xlsx', function() {
+      const validFileName = 'OrderHistory.xlsx';
+      assert.doesNotThrow(excel2JSON.bind(null, validFileName), /InvalidFileTypeError/);
+    });
+    it('should throw an error if file doesnt exist', function() {
+      const nonExistingFileName = 'doesnt_exist.xlsx';
+      assert.throws(excel2JSON.bind(null, nonExistingFileName), /no such file or directory/);
+    });
+    it('should not throw an error if file exists', function() {
+      const existingFileName = 'OrderHistory.xlsx';
+      assert.doesNotThrow(excel2JSON.bind(null, existingFileName), /no such file or directory/);
+    });
+    it('should return an array', function() {
+      const existingFileName = 'OrderHistory.xlsx';
+      const json = excel2JSON(existingFileName);
+      assert.isArray(json);
     });
   });
 });
