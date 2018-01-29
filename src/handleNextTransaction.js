cb4470454f351de7cdcfd71abe4994af260ffd8f
Gerardo Herrera <gherrer1@binghamton.edu>
2018-01-29 01:43:54 -0500

    press next: automatically apply all the changes to your account, layer by layer like version control.

--- src/handleNextTransaction.js
+++ src/handleNextTransaction.js
@@ -4,36 +4,38 @@ export default function handleNextTransaction(event, transactions, balance) {
 
   if(transactions.length === 0) return console.log('No transactions left');
 
-  let nextEarliestTransaction = transactions.shift();
-  if(nextEarliestTransaction.type === 'Withdraw')
-    handleWithdraw(nextEarliestTransaction, balance);
-  else if(nextEarliestTransaction.type === 'Deposit')
-    handleDeposit(nextEarliestTransaction, balance);
-  else if(nextEarliestTransaction.type === 'SELL' || nextEarliestTransaction.type === 'BUY') {
-    let selling =  (nextEarliestTransaction.type === 'SELL');
-    let depositInfo = {
-      currency: nextEarliestTransaction[selling ? 'rhs' : 'lhs'],
-      amount: nextEarliestTransaction[selling ? 'total' : 'filled']
-    };
-    let withdrawInfo = {
-      currency: nextEarliestTransaction[selling ? 'lhs' : 'rhs'],
-      amount: nextEarliestTransaction[selling ? 'filled' : 'total']
-    };
-    handleDeposit(depositInfo, balance);
-    handleWithdraw(withdrawInfo, balance);
-    // fees
-    nextEarliestTransaction.fillDetails.forEach(fillDetail => {
-      console.log('a fill detail');
-      let withdrawInfo = {
-        currency: fillDetail.fee.coin,
-        amount: fillDetail.fee.value
+  while(transactions.length > 0) {
+    let nextEarliestTransaction = transactions.shift();
+    if(nextEarliestTransaction.type === 'Withdraw')
+      handleWithdraw(nextEarliestTransaction, balance);
+    else if(nextEarliestTransaction.type === 'Deposit')
+      handleDeposit(nextEarliestTransaction, balance);
+    else if(nextEarliestTransaction.type === 'SELL' || nextEarliestTransaction.type === 'BUY') {
+      let selling =  (nextEarliestTransaction.type === 'SELL');
+      let depositInfo = {
+        currency: nextEarliestTransaction[selling ? 'rhs' : 'lhs'],
+        amount: nextEarliestTransaction[selling ? 'total' : 'filled']
       };
+      let withdrawInfo = {
+        currency: nextEarliestTransaction[selling ? 'lhs' : 'rhs'],
+        amount: nextEarliestTransaction[selling ? 'filled' : 'total']
+      };
+      handleDeposit(depositInfo, balance);
       handleWithdraw(withdrawInfo, balance);
-    });
-  }
+      // fees
+      nextEarliestTransaction.fillDetails.forEach(fillDetail => {
+        console.log('a fill detail');
+        let withdrawInfo = {
+          currency: fillDetail.fee.coin,
+          amount: fillDetail.fee.value
+        };
+        handleWithdraw(withdrawInfo, balance);
+      });
+    }
 
-  console.log(nextEarliestTransaction);
-  console.log(balance);
+    console.log(nextEarliestTransaction);
+    console.log(balance);
+  }
 }
 
 function handleDeposit({ currency, amount }, balance) {
